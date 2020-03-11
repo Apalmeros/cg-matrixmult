ed4518016e7e49f9039b6fa6180daec1b4e35920
Apalmeros <61282078+Apalmeros@users.noreply.github.com>
2020-03-11 13:27:18 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,18 +8,33 @@ function CalculateCompoundTransform(transforms) {
     // if only one transform, set compound transform eequal to it
     // otherwise multiply all matrices together (in proper order)
     // `compound_transform = Matrix.multiply(...)`
-    var tranform_matrices = [];
-
-    compound_transform = new Matrix(4, 4); // change / remove this
+    var transform_matrices = [];
+	var i;
+	
+	if (transforms.length == 1)
+	{
+		compound_transform = transforms[0].mat4x4;
+	}
+	else
+	{
+		for (i=transforms.length-1; i>=0; i--)
+		{
+			transform_matrices.push(transforms[i].mat4x4);
+		}
+		console.log(transform_matrices);
+		compound_transform = Matrix.multiply(transform_matrices);
+	}
 
     return compound_transform;
+	
 }
 
 // automatically called whenever compound transform changes
 function CalculateTransformedVertex(vertex) {
     // multiple vertex by compound_transform
     // `final_vertex = Matrix.multiply(...)`
-    var final_vertex = new Vector(4); // change / remove this
+    //var final_vertex = new Vector(4); // change / remove this
+	var final_vertex = Matrix.multiply([compound_transform, vertex]);
 
     return final_vertex;
 }
@@ -28,7 +43,26 @@ function CalculateTransformedVertex(vertex) {
 function ChangeTransform(index, type, values) {
     app.transforms[index].type = type;
     // update `app.transforms[index].mat4x4`
-
+	if (type == 'translate')
+	{
+		Mat4x4Translate(app.transforms[index].mat4x4, values[0], values[1], values[2]);
+	}
+	else if (type == 'scale')
+	{
+		Mat4x4Scale(app.transforms[index].mat4x4, values[0], values[1], values[2]);
+	}
+	else if (type == 'rotate_x')
+	{
+		Mat4x4RotateX(app.transforms[index].mat4x4, values[0]);
+	}
+	else if (type == 'rotate_y')
+	{
+		Mat4x4RotateY(app.transforms[index].mat4x4, values[0]);
+	}
+	else
+	{
+		Mat4x4RotateZ(app.transforms[index].mat4x4, values[0]);
+	}
     // recalculate compound transform and tranformed vertex
     app.compound = CalculateCompoundTransform(app.transforms);
     app.final_vertex = CalculateTransformedVertex(app.vertex);
